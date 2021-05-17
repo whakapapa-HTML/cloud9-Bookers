e08b459550bb569e79824d0c6c66faa15dafb1be
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-05-17 07:00:45 +0000

    [Update]3-8 モデルの作成　一覧画面、詳細画面、編集画面の作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
