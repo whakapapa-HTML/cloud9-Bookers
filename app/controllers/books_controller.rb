fd3152fc060f0072cae05af22b76fe2d1e81626d
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-05-17 10:17:49 +0000

    [update] 3-8 バリデーション機能　完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,39 +1,51 @@
 class BooksController < ApplicationController
-  
+
+
+
   def index
     @book = Book.new
-    @books = Book.all
+    @books = Book.all.order(id: :asc)
   end
+
 
   def show
     @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+   if @book.save
+    flash[:notice] = "Book was succeesfully created"
+    redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+     render ("books/index")
+   end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was succeesfully updated"
+    redirect_to book_path(@book.id)
+    else
+    render ("books/edit")
+    end
   end
-  
+
   def destroy
      book = Book.find(params[:id])
      book.destroy
      redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
